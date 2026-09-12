148238c1bc9cff8d3623b02abfae975064d987cd
Semen <semendav@icloud.com>
2026-09-12 22:07:31 +0300

    Сверстан экран профиля пользователя

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Semen Davydov on 12.09.2026.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
+    
+    @IBOutlet weak var profileName: UILabel!
+    
+    
+    @IBOutlet weak var profileUsername: UILabel!
+    
+    @IBOutlet weak var profileStatus: UILabel!
+    
+    @IBAction func exitButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
